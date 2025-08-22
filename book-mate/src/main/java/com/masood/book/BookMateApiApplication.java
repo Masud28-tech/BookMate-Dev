0ee368ef8a66a702c2100e6b23ee7cc1b07b4894
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2025-08-22 17:44:43 +0530

    Initial commit

---
+++ book-mate/src/main/java/com/masood/book/BookMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.masood.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMateApiApplication.class, args);
+	}
+
+}
---
+++ book-mate/src/test/java/com/masood/book/BookMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masood.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
