3dc637780125ad1f16253e5043c8ba96859c805e
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2025-08-28 19:15:21 +0530

    security service filter changes

---
+++ book-mate/src/main/java/com/masood/book/security/JwtFilter.java
@@ -0,0 +1,19 @@
+package com.masood.book.security;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Service
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // JWT validation logic goes here
+        filterChain.doFilter(request, response);
+    }
+}
